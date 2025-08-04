a56632b1ee0713edb701822b1b24c65af5f1afae
ShrinivasChilme <chilmeshrinivas6@gmail.com>
2025-08-04 17:31:29 +0530

    Initial commit

--- src/main/java/com/email/AI_EMAIL_REPLY/EmailGeneratorController.java
+++ src/main/java/com/email/AI_EMAIL_REPLY/EmailGeneratorController.java
@@ -1,9 +1,14 @@
 package com.email.AI_EMAIL_REPLY;
 
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/email")
